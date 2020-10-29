a5342b3b5431596dcc9e1d913fc1a0104fc650f4
Pascal <pascal.baelen@live.com>
2020-10-29 14:25:25 +0100

    orderclassdto

--- src/main/java/com/switchfully/pascal/order/Business/Entity/Order.java
+++ src/main/java/com/switchfully/pascal/order/Business/Entity/Order.java
@@ -1,5 +1,78 @@
 package com.switchfully.pascal.order.Business.Entity;
 
-public class Order {
 
+import java.time.LocalDate;
+import java.util.Objects;
+
+public class Order {
+    public static final Integer TIME_TO_SHIP_DELAYED_IN_WEEKS = 1;
+    private final String emailAddress;
+    private final double totalAmountToPay;
+    private LocalDate dateOfShipMent;
+    private final String selectedItem;
+    private final int wantedAmount;
+
+    public Order(String emailAddress, double totalAmountToPay, LocalDate dateOfShipMent, String selectedItem, int wantedAmount) {
+        this.emailAddress = emailAddress;
+        this.totalAmountToPay = totalAmountToPay;
+        this.dateOfShipMent = LocalDate.now();
+        this.selectedItem = selectedItem;
+        this.wantedAmount = wantedAmount;
+    }
+
+    public static Integer getTimeToShipDelayedInWeeks() {
+        return TIME_TO_SHIP_DELAYED_IN_WEEKS;
+    }
+
+    public LocalDate getDateOfDelayedShipMent() {
+        return dateOfShipMent = LocalDate.now().plusWeeks(TIME_TO_SHIP_DELAYED_IN_WEEKS);
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public double getTotalAmountToPay() {
+        return totalAmountToPay;
+    }
+
+    public LocalDate getDateOfShipMent() {
+        return dateOfShipMent;
+    }
+
+    public String getSelectedItem() {
+        return selectedItem;
+    }
+
+    public int getWantedAmount() {
+        return wantedAmount;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Order order = (Order) o;
+        return Double.compare(order.totalAmountToPay, totalAmountToPay) == 0 &&
+                wantedAmount == order.wantedAmount &&
+                Objects.equals(emailAddress, order.emailAddress) &&
+                Objects.equals(dateOfShipMent, order.dateOfShipMent) &&
+                Objects.equals(selectedItem, order.selectedItem);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(emailAddress, totalAmountToPay, dateOfShipMent, selectedItem, wantedAmount);
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "emailAddress='" + emailAddress + '\'' +
+                ", totalAmountToPay=" + totalAmountToPay +
+                ", dateOfShipMent=" + dateOfShipMent +
+                ", selectedItem='" + selectedItem + '\'' +
+                ", wantedAmount=" + wantedAmount +
+                '}';
+    }
 }
---
+++ src/main/java/com/switchfully/pascal/order/Service/DTO/OrderDTO.java
@@ -0,0 +1,101 @@
+package com.switchfully.pascal.order.Service.DTO;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+public class OrderDTO {
+
+        private Integer TIME_TO_SHIP_DELAYED_IN_WEEKS = 1;
+        private String emailAddress;
+        private double totalAmountToPay;
+        private LocalDate dateOfShipMent;
+        private String selectedItem;
+        private int wantedAmount;
+
+    public Integer getTIME_TO_SHIP_DELAYED_IN_WEEKS() {
+        return TIME_TO_SHIP_DELAYED_IN_WEEKS;
+    }
+
+    public void setTIME_TO_SHIP_DELAYED_IN_WEEKS(Integer TIME_TO_SHIP_DELAYED_IN_WEEKS) {
+        this.TIME_TO_SHIP_DELAYED_IN_WEEKS = TIME_TO_SHIP_DELAYED_IN_WEEKS;
+    }
+
+    public LocalDate getDateOfDelayedShipMent() {
+        return dateOfShipMent = LocalDate.now().plusWeeks(TIME_TO_SHIP_DELAYED_IN_WEEKS);
+    }
+
+    public LocalDate setDateOfDelayedShipMent() {
+        return dateOfShipMent = LocalDate.now().plusWeeks(TIME_TO_SHIP_DELAYED_IN_WEEKS);
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public double getTotalAmountToPay() {
+        return totalAmountToPay;
+    }
+
+    public void setTotalAmountToPay(double totalAmountToPay) {
+        this.totalAmountToPay = totalAmountToPay;
+    }
+
+    public LocalDate getDateOfShipMent() {
+        this.dateOfShipMent = LocalDate.now();
+        return dateOfShipMent;
+    }
+
+    public void setDateOfShipMent(LocalDate dateOfShipMent) {
+        this.dateOfShipMent = LocalDate.now();
+    }
+
+    public String getSelectedItem() {
+        return selectedItem;
+    }
+
+    public void setSelectedItem(String selectedItem) {
+        this.selectedItem = selectedItem;
+    }
+
+    public int getWantedAmount() {
+        return wantedAmount;
+    }
+
+    public void setWantedAmount(int wantedAmount) {
+        this.wantedAmount = wantedAmount;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        OrderDTO orderDTO = (OrderDTO) o;
+        return Double.compare(orderDTO.totalAmountToPay, totalAmountToPay) == 0 &&
+                wantedAmount == orderDTO.wantedAmount &&
+                Objects.equals(TIME_TO_SHIP_DELAYED_IN_WEEKS, orderDTO.TIME_TO_SHIP_DELAYED_IN_WEEKS) &&
+                Objects.equals(emailAddress, orderDTO.emailAddress) &&
+                Objects.equals(dateOfShipMent, orderDTO.dateOfShipMent) &&
+                Objects.equals(selectedItem, orderDTO.selectedItem);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(TIME_TO_SHIP_DELAYED_IN_WEEKS, emailAddress, totalAmountToPay, dateOfShipMent, selectedItem, wantedAmount);
+    }
+
+    @Override
+    public String toString() {
+        return "OrderDTO{" +
+                "TIME_TO_SHIP_DELAYED_IN_WEEKS=" + TIME_TO_SHIP_DELAYED_IN_WEEKS +
+                ", emailAddress='" + emailAddress + '\'' +
+                ", totalAmountToPay=" + totalAmountToPay +
+                ", dateOfShipMent=" + dateOfShipMent +
+                ", selectedItem='" + selectedItem + '\'' +
+                ", wantedAmount=" + wantedAmount +
+                '}';
+    }
+}
