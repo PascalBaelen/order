5dd36b41a23b6b62765218431d9ac116e0d1c9ab
Pascal <pascal.baelen@live.com>
2020-10-30 10:32:12 +0100

    order tests OK

--- src/test/java/com/switchfully/pascal/order/Business/Entity/OrderTest.java
+++ src/test/java/com/switchfully/pascal/order/Business/Entity/OrderTest.java
@@ -40,11 +40,5 @@ class OrderTest {
         Assertions.assertEquals("33a418c6-7a34-4af1-9127-2799ce0c4f31", order.getSelectedItem());
 
     }
-    @Test
-    public void buildNewOrderWithShipmentDate() {
-        Order order = new Order("pascal.baelen@live.com", 334.22,
-                LocalDate.ofYearDay(2020, 322), "33a418c6-7a34-4af1-9127-2799ce0c4f31", 22);
-        Assertions.assertEquals(LocalDate.ofYearDay(2020,322),order.getDateOfShipMent());
 
-    }
 }
